50f452c8f7ed52865295a701b90edf86f7698309
Nick <1825545+nickkitovsky@users.noreply.github.com>
2025-06-03 00:34:45 +0700

    remove update_server_connection_time

--- src/prober.py
+++ src/prober.py
@@ -3,13 +3,12 @@ import contextlib
 import logging
 import time
 
-from .schemas import Server
-
 logger = logging.getLogger(__name__)
 
 
-async def _get_connection_time(
-    server: Server,
+async def get_connection_time(
+    address: str,
+    port: int,
     timeout: float = 1.0,
     max_concurrent: int = 50,
 ) -> float | None:
@@ -18,31 +17,11 @@ async def _get_connection_time(
         with contextlib.suppress(asyncio.TimeoutError, OSError):
             _, writer = await asyncio.wait_for(
                 asyncio.open_connection(
-                    server.connection_details.address,
-                    server.connection_details.port,
+                    address,
+                    port,
                 ),
                 timeout=timeout,
             )
             writer.close()
             await writer.wait_closed()
             return round(time.time() - start_time, 3)
-
-
-async def update_server_connection_time(
-    server: Server,
-    timeout: float = 1.0,
-    max_concurrent: int = 50,
-) -> None:
-    connection_time = await _get_connection_time(server, timeout, max_concurrent)
-    if connection_time:
-        server.connection_time = connection_time
-        logger.debug(
-            "Connection time to %s: %s",
-            server.connection_details.address,
-            connection_time,
-        )
-    else:
-        logger.warning(
-            "Failed to get connection time for server %s",
-            server.connection_details.address,
-        )
